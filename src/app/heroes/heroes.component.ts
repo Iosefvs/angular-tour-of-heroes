86793ff2f348bd8d0deb90b1a0f74be18107b14f
Iosefvs <jose.m.gallego@gmail.com>
2023-05-26 11:23:11 +0200

    Ejercicio finalizado

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -47,4 +47,27 @@ export class HeroesComponent implements OnInit{
     this.heroService.getHeroes()
         .subscribe(heroes => this.heroes = heroes);
   }
+
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.addHero({ name } as Hero)
+      .subscribe(hero => {
+        this.heroes.push(hero);
+      });
+  }
+
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter(h => h !== hero);
+    /*
+    If you neglect to subscribe(), the service can't send the delete request to the server. 
+    As a rule, an Observable does nothing until something subscribes.
+    Confirm this for yourself by temporarily removing the subscribe(), clicking Dashboard, then clicking Heroes. 
+    This shows the full list of heroes again.
+    */
+    this.heroService.deleteHero(hero.id).subscribe();
+
+
+  }
+
 }
